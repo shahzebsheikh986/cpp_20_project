02af9174471d8c56bef2b5cb836a026ef7ed9067
shahzebsheikh986 <shahzebsheikh986@gmail.com>
2024-11-15 19:44:32 +0530

    new change

---
+++ 02_Diving in/ 3.2_Comments/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+//One line comment
+
+//This brings in the iostream library
+
+/*
+    This is a block comment that englobes multiple
+    lines of text
+*/
+cout<<"hello world";
+    return 0;
+}
---
+++ 02_Diving in/ 3.3_Errors_and_warnings/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+ //Compile time error
+   std::cout << "Hello Word!" << std::endl;
+
+   //Run time error
+   int value = 7/0;
+   std::cout << "value : " << value << std::endl;
+    return 0;
+}
